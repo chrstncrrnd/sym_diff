c5b991847200dd5006e052b99d1bf24823c7233e
chrstncrrnd <chrstncrrnd@gmail.com>
2026-04-16 17:39:08 +0200

    lexer is mostly done

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use crate::tokenizer::Lexer;
 mod tokenizer;
 
 fn main() {
-    let mut buf: String = String::new();
-    println!("Expression to differentiate: ");
-    io::stdin().read_line(&mut buf).unwrap();
-    let a = buf.trim().chars().peekable();
+  let mut buf: String = String::new();
+  println!("Expression to differentiate: ");
+  io::stdin().read_line(&mut buf).unwrap();
+  let a = buf.trim().chars().peekable();
 
-    let mut lexer = Lexer::new(a);
-    println!("Lexer one: {:?}, two: {:?}", lexer.next(), lexer.next());
+  let mut lexer = Lexer::new(a);
+  println!("Lexer one: {:?}, two: {:?}", lexer.next(), lexer.next());
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,54 +2,101 @@ use std::{iter::Peekable, str::Chars};
 
 #[derive(Debug)]
 pub enum Token {
-    Var, // TODO: add functions
-    Num(f64),
-    Plus,
-    Minus,
-    Mult,
-    Div,
-    Power,
-    LParen, // groupings
-    RParen,
-    Eol, //end of line
+  Var, // TODO: add functions
+  Num(f64),
+  Plus,
+  Minus,
+  Mult,
+  Div,
+  Power,
+  LParen, // groupings
+  RParen,
+  Err(String),
+}
+
+enum LexerState {
+  Initial,
+  Asterisk,
+  Digit,
 }
 
 #[derive(Debug)]
 pub struct Lexer<'a> {
-    input: Peekable<Chars<'a>>,
-    current_token: String,
+  input: Peekable<Chars<'a>>,
 }
 
 impl<'a> Lexer<'a> {
-    pub fn new(input: Peekable<Chars<'a>>) -> Self {
-        Self {
-            input,
-            current_token: String::new(),
-        }
-    }
+  pub fn new(input: Peekable<Chars<'a>>) -> Self {
+    Self { input }
+  }
 }
 
 impl Iterator for Lexer<'_> {
-    type Item = Token;
+  type Item = Token;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if let Some(c) = self.input.peek() {
-                if *c == 'x' {
-                    return Some(Token::Var);
-                } else if c.is_ascii_digit() {
-                    if let Some(next) = self.input.peek()
-                        && next.is_ascii_digit()
-                    {
-                        self.current_token = format!("{}{}", self.current_token, c);
-                        continue;
-                    } 
-                } else {
-                    return None;
-                }
-            } else {
-                return None;
+  fn next(&mut self) -> Option<Self::Item> {
+    let mut state: LexerState = LexerState::Initial;
+    let mut c: Option<char>;
+    let mut buf = String::new();
+
+    loop {
+      c = self.input.next();
+      match state {
+        LexerState::Initial => {
+          // returns none if this is none
+        let ch = c?;
+          if ch.is_ascii_digit() {
+            if ch == '0' {
+              return Some(Token::Err("Got a number starting with 0!".to_string()));
             }
+            buf = format!("{}{}", buf, ch);
+            state = LexerState::Digit;
+            continue;
+          }
+
+          if ch == '*' {
+            let next = self.input.peek();
+            if let Some(nch) = next
+              && *nch == '*'
+            {
+              state = LexerState::Asterisk;
+              continue;
+            }
+            return Some(Token::Mult);
+          }
+
+          return match ch {
+            '+' => Some(Token::Plus),
+            '-' => Some(Token::Minus),
+            '(' => Some(Token::LParen),
+            ')' => Some(Token::RParen),
+            '/' => Some(Token::Div),
+            'x' => Some(Token::Var),
+            _ => Some(Token::Err("Unexpected character!".to_string())),
+          };
         }
+        LexerState::Digit => {
+          let ch = c.unwrap();
+          if ch.is_ascii_digit() {
+            buf = format!("{}{}", buf, ch);
+          }
+          let next = self.input.peek();
+          if let Some(nch) = next {
+            if !(*nch).is_ascii_digit() {
+              return Some(Token::Num(buf.parse().unwrap()));
+            }
+          } else {
+            return Some(Token::Num(buf.parse().unwrap()));
+          }
+        }
+        LexerState::Asterisk => {
+        if let Some(ch) = c && ch == '*'{
+            return Some(Token::Power);
+        }else{
+            return Some(Token::Err("Unexpected character!".to_string()));
+        }
+        }
+      }
     }
+  }
 }
